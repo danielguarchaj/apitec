7232a454bcca028d33d1095f000760611ad468db
Daniel <gtdanniel@gmail.com>
2019-05-28 16:41:50 -0600

    Change assignment activity str

--- academy/models.py
+++ academy/models.py
@@ -70,7 +70,7 @@ class AssignmentActivity(models.Model):
     )
 
     def __str__(self):
-        return f'{self.pk} - {self.activity.title} {self.created} exp: {self.deadline}'
+        return f'{self.pk} - {self.activity.title} exp: {self.deadline} - Group: {self.group}'
 
     class Meta:
         verbose_name = 'Assignment activity'
